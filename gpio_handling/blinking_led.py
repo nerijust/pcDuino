7d2b3ad07ab44386cb1f5105a868a614719c6405
Nerijus <nerijust@gmail.com>
2013-06-20 19:35:44 +0300

    Update blinking_led.py

--- gpio_handling/blinking_led.py
+++ gpio_handling/blinking_led.py
@@ -9,19 +9,13 @@ import time, os
 from gpio import *
 
 
-def setup():
-    GPIO_MODE(2, OUTPUT) # make GPIO2 pin an output
-    GPIO_DATA(2, LOW)    # set it LOW
+GPIO_MODE(2, OUTPUT) # make GPIO2 pin an output
+GPIO_DATA(2, LOW)    # set it LOW
 
-def loop():
-    while(1):
-        GPIO_DATA(2, HIGH) # set GPIO2 pin HIGH
-        time.sleep(2)      # wait 2 sec.
-        GPIO_DATA(2, LOW)  # set GPIO2 pin LOW
-        time.sleep(2)      # wait 2 sec.
+while(1):
+    GPIO_DATA(2, HIGH) # set GPIO2 pin HIGH
+    time.sleep(2)      # wait 2 sec.
+    GPIO_DATA(2, LOW)  # set GPIO2 pin LOW
+    time.sleep(2)      # wait 2 sec.
 
-def main():
-    setup()
-    loop()
 
-main()
