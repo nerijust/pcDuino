2413e9b0353e2ff15fa245c042fbc2d90ed2d3f4
Nerijus <nerijust@gmail.com>
2013-06-26 23:21:22 +0300

    Update ntc_temp_read.py

--- gpio_handling/ntc_temp_read.py
+++ gpio_handling/ntc_temp_read.py
@@ -10,7 +10,7 @@ from gpio import *
 from math import log
 
 def loop():
-    V_in = 3.3 #voltage in to voltage divider
+	V_in = 3.3 #voltage in to voltage divider
 	ADC_resoliution = V_in/4095 #12 bit resolution in V
 	R_nom = 1000 #nominal NTC termistor resistance at nominal temperature (25 deg Celsius)
 	R_sec = 1000 #resistance of second resistor in voltage divider
@@ -24,6 +24,7 @@ def loop():
 		log_value = R_actual/R_nom #log function value for next formula
 		temp_K = (B * T_nom)/(B + (math.log(log_value) * T_nom)) #actual temperature in Kelvin
 		temp = temp_K - 273.15 #actual temperature in Celsius
+
 		print 'temperature: ', "%.2f" % temp, 'deg C'
 		time.sleep(1)      # wait 1 sec.
 		
