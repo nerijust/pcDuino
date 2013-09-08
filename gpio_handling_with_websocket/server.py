b134ba71648841746a7464fe095276b038967644
Nerijus <nerijust@gmail.com>
2013-09-08 10:50:35 +0300

    Update server.py

--- gpio_handling_with_websocket/server.py
+++ gpio_handling_with_websocket/server.py
@@ -9,33 +9,41 @@ import os
 from gpio import *
 from twisted.internet import reactor
 from twisted.python import log
-
+ 
 from autobahn.websocket import WebSocketServerFactory, \
                                WebSocketServerProtocol, \
                                listenWS
-                               
+							   
 for i in range (14): 
-	GPIO_MODE(i, OUTPUT)
+	GPIO_MODE(i, OUTPUT) 						# make all GPIO output.
 
 for i in range (14):
-	GPIO_DATA(i, LOW)
+	GPIO_DATA(i, LOW) 							# make all GPIO low.
 
 class EchoServerProtocol(WebSocketServerProtocol):
  
     def onMessage(self, msg, binary):
-		pin = msg.split(':')[0]
-		data = msg.split(':')[1]
-		GPIO_DATA(pin, data)
-		print "sending echo:", msg
-		self.sendMessage(msg, binary)
+	
+		if msg == "refresh": 					# on message "refresh" check GPIO status and send info to client
+			for i in range (0,14):
+				status = str(GPIO_STATUS(i))
+				msg = str(i)+":"+status
+				print "sending echo:", msg
+				self.sendMessage(msg, binary)
+				
+				
+		else: 									# at now client send only two types of messages 
+			a = msg.split(':')[0]				# "refresh" and request to change GPIO status.
+			b = msg.split(':')[1]				# So here is code for change GPIO status (ON/OFF).
+			GPIO_DATA(a, b)
+			#print "sending echo:", msg
+			#self.sendMessage(msg, binary)
 		
-
- 
 if __name__ == '__main__':
- 
+
    log.startLogging(sys.stdout)
  
-   factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
+   factory = WebSocketServerFactory("ws://192.168.1.4:9000", debug = False)
    factory.protocol = EchoServerProtocol
    listenWS(factory)
  
